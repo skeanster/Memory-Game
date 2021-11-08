1ed6730e29c88419f653cde335e3e7040a4df313
skeanster <ryanskeans@gmail.com>
2021-11-08 08:01:16 -0800

    added footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './components/header';
 import Main from './components/main';
+import Footer from './components/footer';
 
 const App = () => {
   return (
     <div>
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 };
---
+++ src/components/footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="App-footer">
+      <div>
+        Explore the source code:{' '}
+        <a
+          href="https://github.com/skeanster/Memory-Game"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
